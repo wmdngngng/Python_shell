ae3f7d737949a0cd204704def09bd8ac5e04c340
xqt2010a <2367334102@qq.com>
2017-06-30 01:23:10 +0800

    1. 串口扫描 2. 串口发送

---
+++ 03_pyserial.py
@@ -0,0 +1,26 @@
+# 说明：python pyserial study
+# 1. 
+# 2. 
+# 3. 
+# 4. 
+# 5. 
+# 6. 
+#
+# @Author: labc
+# Created on June 30 2017
+#
+#
+import serial
+import serial.tools.list_ports
+
+PortList = list(serial.tools.list_ports.comports())
+if len(PortList) <= 0:
+    print ("the serial port can not find!")
+else:
+    print ("serial port num:", len(PortList))
+    portlist0 = list(PortList[0])
+    portserial = portlist0[0]
+    print (portserial)
+    ser = serial.Serial(portserial,9600,timeout = 60)
+    print ("The port is really use>",ser.name)
+    ser.write(b"Hello World!\r\n")
