8d429962ac75830af49511a5467fdb719f16c64d
xqt2010a <2367334102@qq.com>
2017-07-07 00:54:48 +0800

    添加了文本框，单选框，数码显示，码表，横竖刻度条UI。

--- 06_PyQt5_Eric6/01_test.py
+++ 06_PyQt5_Eric6/01_test.py
@@ -64,9 +64,85 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         Slot documentation goes here.
         """
         # TODO: not implemented yet
-        print (self.lineEdit.text())
+        #print (self.lineEdit.text())
+        input_string = self.lineEdit.text()
+        self.textBrowser.append(input_string)
 
+    @pyqtSlot()
+    def on_pushButton_9_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        #raise NotImplementedError
+        b9_string = self.textBrowser.toPlainText()
+        print(b9_string)
+    
+    @pyqtSlot()
+    def on_pushButton_10_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        self.textBrowser.clear()
 
+    @pyqtSlot()
+    def on_radioButton_ty_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        ty_string = "你选择了同意"
+        print(ty_string)
+        self.textBrowser.append(ty_string)
+        self.radioButton_jb.setChecked(True)
+    
+    @pyqtSlot()
+    def on_radioButton_bty_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        bty_string = "你选择了不同意"
+        print(bty_string)
+        self.textBrowser.append(bty_string)
+        self.radioButton_bjb.setChecked(True)
+        
+    @pyqtSlot()
+    def on_radioButton_jb_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        jb_string = "你选择了加班"
+        print(jb_string)
+        self.textBrowser.append(jb_string)
+    
+    @pyqtSlot()
+    def on_radioButton_bjb_clicked(self):
+        """
+        Slot documentation goes here.
+        """
+        # TODO: not implemented yet
+        bjb_string = "你选择了不加班"
+        print(bjb_string)
+        self.textBrowser.append(bjb_string)
+
+    @pyqtSlot(int)
+    def on_dial_valueChanged(self, value):
+        """
+        Slot documentation goes here.
+        
+        @param value DESCRIPTION
+        @type int
+        """
+        # TODO: not implemented yet
+        #self.textBrowser.append(self, str=value)
+        print(value)
+        self.lcdNumber.display(value*10.3)
+        print(value*10.3)
+        self.horizontalSlider.setValue(value)
+        self.verticalSlider.setValue(value)
 
 if __name__ == "__main__":
     import sys
@@ -75,4 +151,3 @@ if __name__ == "__main__":
     ui.show()
     sys.exit(app.exec_())
     
-
