acf78166ad661be3c3aea72b491e754aa38cd2da
xqt2010a <2367334102@qq.com>
2017-07-27 02:10:55 +0800

    add single and slot RTT data view on textBrowser success!

--- 07_JLink_RTT/JLink_RTT.py
+++ 07_JLink_RTT/JLink_RTT.py
@@ -4,10 +4,10 @@
 Module implementing MainWindow.
 """
 import ctypes,  struct
-import os, configparser
+import os, configparser,  time,  threading
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QMainWindow, QFileDialog
-from PyQt5 import  QtWidgets
+from PyQt5 import  QtWidgets,  QtCore
 from Ui_JLink_RTT import Ui_MainWindow
 
 class SeggerRttBuf(object):
@@ -18,6 +18,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     """
     Class documentation goes here.
     """
+    received = QtCore.pyqtSignal(str)
     def __init__(self, parent=None):
         """
         Constructor
@@ -25,9 +26,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         @param parent reference to the parent widget
         @type QWidget
         """
+        self.connect_flag = False
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.initSetting()
+        self.received.connect(self.textBrowser_view)
+        threading.Thread(target=self.jlink_read).start()
+        
         
     def initSetting(self):
         self.settingPath = "../setting.ini"
@@ -41,6 +46,64 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 
         else:
             self.textBrowser("Error:setting.ini not found.")
+    def upBuff0Empty(self):
+        self.RttAddr = 0x100104e4
+        b_len = 168
+        buf = ctypes.create_string_buffer(b_len)
+        self.jlink.JLINKARM_ReadMem(self.RttAddr, b_len, buf)
+        acID, MaxNumUpBuf, MaxNumDownBuf = struct.unpack("16sii", buf.raw[:24])
+        upBuffEnd = 24+24*1
+        downBuffStart = 24+24*MaxNumUpBuf
+        downBuffEnd = downBuffStart+24
+        upBuffs0 = struct.unpack("PPiiii", buf.raw[24:upBuffEnd])
+        downBuffs0 = struct.unpack("PPiiii", buf.raw[downBuffStart:downBuffEnd])
+        self.upBuff0 = SeggerRttBuf(upBuffs0)
+        self.downBuff0 = SeggerRttBuf(downBuffs0)
+        print("sName:%x"%(self.upBuff0.sName))
+        print("pBuffer:%x"%(self.upBuff0.pBuffer))
+        print("SizeOfBuffer:%d"%(self.upBuff0.SizeOfBuffer))
+        print("WrOff:%d"%(self.upBuff0.WrOff))
+        print("RdOff:%d"%(self.upBuff0.RdOff))
+        print("Flags:%d"%(self.upBuff0.Flags))
+        return (self.upBuff0.RdOff == self.upBuff0.WrOff)
+        
+    def upBuff0Read(self):
+        if self.upBuff0.RdOff < self.upBuff0.WrOff:
+            len = self.upBuff0.WrOff - self.upBuff0.RdOff
+            str = ctypes.create_string_buffer(len)
+            raddr = self.upBuff0.pBuffer + self.upBuff0.RdOff
+            self.jlink.JLINKARM_ReadMem(raddr, len, str)
+            self.upBuff0.RdOff += len
+            pRdOff_len = struct.pack("L", self.upBuff0.RdOff)
+            buf = ctypes.create_string_buffer(pRdOff_len)
+            rdoff_addr = self.RttAddr + 16+4*2+4*4
+            self.jlink.JLINKARM_WriteMem(rdoff_addr, 4, buf)
+        else:
+            len = self.upBuff0.SizeOfBuffer - self.upBuff0.RdOff + 1
+            str = ctypes.create_string_buffer(len)
+            raddr = self.upBuff0.pBuffer + self.upBuff0.RdOff
+            self.jlink.JLINKARM_ReadMem(raddr, len, str)
+            self.upBuff0.RdOff = 0;
+            pRdOff_len = struct.pack("L", self.upBuff0.RdOff)
+            buf = ctypes.create_string_buffer(pRdOff_len)
+            rdoff_addr = self.RttAddr+16+4*2+4*4
+            self.jlink.JLINKARM_WriteMem(rdoff_addr, 4, buf)
+            print("s:", pRdOff_len)
+            print("rdoff_addr:%x"%(rdoff_addr))
+        return str.raw.decode("utf_8")
+        
+    def jlink_read(self):
+        print("jlink_read")
+        while True:
+            while self.connect_flag == True:
+                print("connect_flag:", self.connect_flag)
+                if not self.upBuff0Empty():
+                    self.received.emit(self.upBuff0Read())
+                time.sleep(0.9)
+        
+    def textBrowser_view(self, str):
+        self.textBrowser.append(str)
+        #self.textBrowser.append(str.decode("utf_8"))  #ascii    
         
     @pyqtSlot()
     def on_toolButton_jlink_lj_clicked(self):
@@ -84,58 +147,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             print("123:")
             #str = "device=CR600"
             #str = ctypes.c_wchar_p("device=CR600")
-            #print("exe:", self.jlink.JLINK_ExecCommand(ctypes.c_char_p("device=CR600")))
+            #str = ctypes.c_wchar("device=CR600")
+            #print("exe:", self.jlink.JLINK_ExecCommand(str))
+            #self.jlink.JLINK_ExecCommand(b"device=CR600")
+            #pbuf_len = struct.pack("12s", "device=CR600")
+            #buf = ctypes.create_string_buffer(pbuf_len)
+            #val = self.jlink.JLINK_ExecCommand(buf)
+            #print("var:", val)
             print("isopen:", isopen)
-            RttAddr = 0x100104e4
-            b_len = 168
-            buf = ctypes.create_string_buffer(b_len)
-            self.jlink.JLINKARM_ReadMem(RttAddr, b_len, buf)
-            acID, MaxNumUpBuf, MaxNumDownBuf = struct.unpack("16sii", buf.raw[:24])
-            upBuffEnd = 24+24*1
-            downBuffStart = 24+24*MaxNumUpBuf
-            downBuffEnd = downBuffStart+24
-            upBuffs0 = struct.unpack("PPiiii", buf.raw[24:upBuffEnd])
-            downBuffs0 = struct.unpack("PPiiii", buf.raw[downBuffStart:downBuffEnd])
-            self.upBuff0 = SeggerRttBuf(upBuffs0)
-            self.downBuff0 = SeggerRttBuf(downBuffs0)
-            print("sName:%x"%(self.upBuff0.sName))
-            print("pBuffer:%x"%(self.upBuff0.pBuffer))
-            print("SizeOfBuffer:%d"%(self.upBuff0.SizeOfBuffer))
-            print("WrOff:%d"%(self.upBuff0.WrOff))
-            print("RdOff:%d"%(self.upBuff0.RdOff))
-            print("Flags:%d"%(self.upBuff0.Flags))
-            
-            if self.upBuff0.RdOff < self.upBuff0.WrOff:
-                len = self.upBuff0.WrOff - self.upBuff0.RdOff
-                str = ctypes.create_string_buffer(len)
-                raddr = self.upBuff0.pBuffer + self.upBuff0.RdOff
-                self.jlink.JLINKARM_ReadMem(raddr, len, str)
-                self.upBuff0.RdOff += len
-                pRdOff_len = struct.pack("L", self.upBuff0.RdOff)
-                buf = ctypes.create_string_buffer(pRdOff_len)
-                rdoff_addr = RttAddr + 16+4*2+4*4
-                self.jlink.JLINKARM_WriteMem(rdoff_addr, 4, buf)
-                
-            else:
-                len = self.upBuff0.SizeOfBuffer - self.upBuff0.RdOff + 1
-                str = ctypes.create_string_buffer(len)
-                raddr = self.upBuff0.pBuffer + self.upBuff0.RdOff
-                self.jlink.JLINKARM_ReadMem(raddr, len, str)
-                self.upBuff0.RdOff = 0;
-                pRdOff_len = struct.pack("L", self.upBuff0.RdOff)
-                buf = ctypes.create_string_buffer(pRdOff_len)
-                rdoff_addr = RttAddr+16+4*2+4*4
-                self.jlink.JLINKARM_WriteMem(rdoff_addr, 4, buf)
-                print("ebuf:", buf)
-                print("s:", pRdOff_len)
-                print("rdoff_addr:%x"%(rdoff_addr))
-             
-            print("str:", str)
-            print("str.raw:", str.raw)
-            print("str.value:", str.value)
-            real_str = str.raw.decode("utf_8")
-            self.textBrowser.append("hello")
-            self.textBrowser.append(str.raw.decode("utf_8"))  #ascii
+            str = ctypes.create_string_buffer(4)
+            self.jlink.JLINKARM_ReadMem(0x10000000, 4, str)
+            self.connect_flag = True
               
     @pyqtSlot()
     def on_pushButton_clear_clicked(self):
@@ -145,6 +167,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # TODO: not implemented yet
         self.textBrowser.clear()
         self.jlink.JLINKARM_Close()
+        
 
 if __name__ == "__main__":
     import sys
