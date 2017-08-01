d16b4728f47d7ded026ae3204fa3b595fc2ae7d6
xqt2010a <2367334102@qq.com>
2017-08-02 00:15:11 +0800

    add upBuffEmpty and upBuffRead function.

--- 07_JLink_RTT/RTT.py
+++ 07_JLink_RTT/RTT.py
@@ -6,16 +6,17 @@ class RttBuf(object):
         self.sName,  self.pBuffer,  self.SizeOfBuffer,  self.WrOff,  self.RdOff,  self.Flags = arr
 
 class RttCB(object):
-    def __init__(self, buf):
+    def __init__(self, buf, upport, downport):
         self.acID, self.MaxNumUpBuf,  self.MaxNumDownBuf  = struct.unpack("16sii", buf.raw[:24])
         print("len:", len(buf.raw[24:(48+0*24)]))
         for i in range(0, self.MaxNumUpBuf):
-            print("i", i)
-            self.aUp = RttBuf(struct.unpack("6L", buf.raw[(24+24*i):(48+24*i)]))
+            if (i == upport):
+                self.aUp = RttBuf(struct.unpack("6L", buf.raw[(24+24*i):(48+24*i)]))
         
         aDown_start = 48+i*24
         for i in range(0, self.MaxNumDownBuf):
-            self.aDown = RttBuf(struct.unpack("6L", buf.raw[(aDown_start + 24*i): (aDown_start+24+24*i)]))
+            if(i == downport):
+                self.aDown = RttBuf(struct.unpack("6L", buf.raw[(aDown_start + 24*i): (aDown_start+24+24*i)]))
             
         
 class RTT(object):
@@ -23,23 +24,25 @@ class RTT(object):
     def __init__(self, parent=None):
         print("hello")
         
-    def upBuff0Empty(self, rtt_addr, len):
+    def upBuffEmpty(self, rtt_addr, len, upport, downport):
         buf = ctypes.create_string_buffer(len)
         self.jlink.JLINKARM_ReadMem(rtt_addr, len, buf)
-        rtt_cb = RttCB(buf)
-        print(rtt_cb.aUp[0].sName)
+        self.rtt_cb = RttCB(buf, upport, downport)
+        return self.rtt_cb.aUp.RdOff == self.rtt_cb.aUp.WrOff
         
-        upBuffEnd = 24+24*1
-        downBuffStart = 24+24*MaxNumUpBuf
-        downBuffEnd = downBuffStart+24
-        upBuffs0 = struct.unpack("PPiiii", buf.raw[24:upBuffEnd])
-        downBuffs0 = struct.unpack("PPiiii", buf.raw[downBuffStart:downBuffEnd])
-        self.upBuff0 = RttBuf(upBuffs0)
-        self.downBuff0 = RttBuf(downBuffs0)
-        print("sName:%x"%(self.upBuff0.sName))
-        print("pBuffer:%x"%(self.upBuff0.pBuffer))
-        print("SizeOfBuffer:%d"%(self.upBuff0.SizeOfBuffer))
-        print("WrOff:%d"%(self.upBuff0.WrOff))
-        print("RdOff:%d"%(self.upBuff0.RdOff))
-        print("Flags:%d"%(self.upBuff0.Flags))
-        return (self.upBuff0.RdOff == self.upBuff0.WrOff)
+    def upBuffRead(self, rtt_addr, upport,  downport):
+        if self.rtt_cb.aUp.RdOff < self.rtt_cb.aUp.WrOff:
+            len = self.rtt_cb.aUp.WrOff - self.rtt_cb.aUp.RdOff
+            buf = ctypes.create_string_buffer(len)
+            self.jlink.JLINKARM_ReadMem(self.rtt_cb.aUp.pBuffer+self.rtt_cb.aUp.RdOff,  len,  buf)
+            self.rtt_cb.aUp.RdOff += len
+            str = ctypes.create_string_buffer(struct.pack("L", self.rtt_cb.aUp.RdOff))
+            self.jlink.JLINKARM_WriteMem(rtt_addr+24+upport*24+16, 4, str)
+        else:
+            len = self.rtt_cb.aUp.SizeOfBuffer - self.rtt_cb.aUp.RdOff +1
+            buf = ctypes.create_string_buffer(len)
+            self.jlink.JLINKARM_ReadMem(self.rtt_cb.aUp.pBuffer+self.rtt_cb.aUp.RdOff, len, buf)
+            self.rtt_cb.aUp.RdOff = 0
+            str = ctypes.create_string_buffer(struct.pack("L", self.rtt_cb.aUp.RdOff))
+            self.jlink.JLINKARM_WriteMem(rtt_addr+24+upport*24+16, 4, str)
+            
