f6c88e3032b14bc2bdde6b4000b2df9c471c7b53
xqt2010a <2367334102@qq.com>
2017-08-01 23:02:15 +0800

    add RttBuf and RttCB

--- 07_JLink_RTT/RTT.py
+++ 07_JLink_RTT/RTT.py
@@ -1,13 +1,22 @@
 import ctypes, struct
-import numpy as np
+#import numpy as np
 
 class RttBuf(object):
     def __init__(self, arr):
         self.sName,  self.pBuffer,  self.SizeOfBuffer,  self.WrOff,  self.RdOff,  self.Flags = arr
 
-class UpDownBuf(object):
-    def __init__(self, upNum, downNum):
-        self.
+class RttCB(object):
+    def __init__(self, buf):
+        self.acID, self.MaxNumUpBuf,  self.MaxNumDownBuf  = struct.unpack("16sii", buf.raw[:24])
+        print("len:", len(buf.raw[24:(48+0*24)]))
+        for i in range(0, self.MaxNumUpBuf):
+            print("i", i)
+            self.aUp = RttBuf(struct.unpack("6L", buf.raw[(24+24*i):(48+24*i)]))
+        
+        aDown_start = 48+i*24
+        for i in range(0, self.MaxNumDownBuf):
+            self.aDown = RttBuf(struct.unpack("6L", buf.raw[(aDown_start + 24*i): (aDown_start+24+24*i)]))
+            
         
 class RTT(object):
     "RTT api"
@@ -17,13 +26,8 @@ class RTT(object):
     def upBuff0Empty(self, rtt_addr, len):
         buf = ctypes.create_string_buffer(len)
         self.jlink.JLINKARM_ReadMem(rtt_addr, len, buf)
-        acID, MaxNumUpBuf, MaxNumDownBuf = struct.unpack("16sii", buf.raw[:24])
-        rttUpDownBuf = np.dtype({
-            "names":["aUp", "aDown"]
-            "formats":["S16", "i", "i"]
-        
-            })
-        print(RTT_CB)
+        rtt_cb = RttCB(buf)
+        print(rtt_cb.aUp[0].sName)
         
         upBuffEnd = 24+24*1
         downBuffStart = 24+24*MaxNumUpBuf
