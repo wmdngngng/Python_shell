7fb4436bad94d88b2b030c7045e95406c8acd142
nanchang <chang.nan@nextvpu.com>
2018-09-21 20:47:14 +0800

    sd write read 512bytes test ok. note: the write read size must be 512bytes.

--- 13_disk/01_base.py
+++ 13_disk/01_base.py
@@ -6,38 +6,89 @@ import win32con
 
 
 def get_disk_info():
-    """
-    :return: get the disk info
-    """
-    tmplist = []
     c = wmi.WMI()
-    for physical_disk in c.Win32_DiskDrive():
-        tmpdict ={}
-        #tmpdict["FreeSpace"] = physical_disk.Signature
-        tmpdict["Caption"] = physical_disk.Caption
-        tmpdict["Size"] = int(physical_disk.Size)/1024/1024/1024
-        tmplist.append(tmpdict)
-    return tmplist
-    
+    for disk in c.Win32_DiskDrive():
+        print("01", disk.Index)     #0,1,2
+        print("02", int(disk.Size)/1024/1024/1024)
+        print("03", disk.MediaType) #Fixed hard disk media  #Removable Media, usb and sd
+        print("04", disk.DeviceID)
+        print("05", disk.Name)
+        print("06", disk.Caption)
+        print("07", disk.Model)
+        print("08", disk.InterfaceType)
+        print("09", disk.BytesPerSector)
+        print("10", disk.TotalSectors)
+        print("11", disk.FirmwareRevision)
+        print("12", disk.Partitions)
+        print("13", disk.TotalTracks)
+        print("14", disk.TotalCylinders)
+        print("15", disk.SerialNumber)
+        print("16", disk.PNPDeviceID)
+        
+        print("17", disk.Description)
+        print("18", disk.Manufacturer)
+        print("19", disk.CreationClassName)
+        print("20", disk.SystemCreationClassName)
+        print("21", disk.SystemName)
+        print("22", disk.TotalHeads)
+        print("23", disk.SectorsPerTrack)
+        print("24", disk.TracksPerCylinder)
+        print("25", disk.MediaLoaded)
+        print("26", disk.Status)
+        print("\n")
+        
 def get_logic_info():
-    #templist = []
     c = wmi.WMI()
     for logic in c.Win32_LogicalDisk():
         if logic.Size != None:
             print("01",logic.Caption)    #打印盘符
             print("02",int(logic.Size)/1024/1024/1024)
             print("10",logic.Description)
-            print("12",logic.DeviceID)
+            print("11",logic.DeviceID)
             print("12",logic.DriveType)
             print("13",logic.FileSystem)
             print("17",logic.MediaType)
+            print("18",logic.SerialNumber)
             print("35",logic.VolumeName)
             print("\n")
-    #tmpdict["xxx"] = 
-    #templist.append(tmpdict)
-    
-
+            
 def get_removable_disk():
+    index = 0
+    disks_list = []
+    c = wmi.WMI()
+    for disk in c.Win32_DiskDrive():
+        if disk.MediaType == "Removable Media":
+            tempdisk = {}
+            index = index + 1
+            print("Index=%d Disk=%d" %(index, disk.Index), disk.InterfaceType,
+                "%5.1fG" %(int(disk.Size)/1024/1024/1024))
+            tempdisk["index"] = index
+            tempdisk["disk"] = disk.Index
+            tempdisk["ID"] = disk.DeviceID
+            disks_list.append(tempdisk)
+    sel_index = input("Please Input Index:")
+    for disk_list in disks_list:
+        if disk_list["index"] == int(sel_index):
+            print("select Disk=%d"%disk_list["disk"])
+            disk_name = disk_list["ID"][4:]
+            #print(disk_name)
+            hfile=win32file.CreateFile("\\\\.\\"+disk_name, win32con.GENERIC_WRITE, 0,None, win32con.OPEN_EXISTING, 0 , None)
+            data = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+            data1 = data + data
+            data2 = data1 + data1
+            data3 = data2 + data2
+            b_hex = data3.encode('ascii')
+            print(b_hex)
+            win32file.WriteFile(hfile, b_hex)
+            hfile.Close()
+            hfile=win32file.CreateFile("\\\\.\\"+disk_name, win32con.GENERIC_READ|win32con.GENERIC_WRITE,
+                win32con.FILE_SHARE_READ|win32con.FILE_SHARE_WRITE,
+                None, win32con.OPEN_EXISTING, 0 , None)
+            result, data = win32file.ReadFile(hfile, 512)
+            print(result,data)
+            hfile.Close()
+            
+def get_removable_logical():
     c = wmi.WMI()
     for disk in c.Win32_LogicalDisk():
         if disk.DriveType == 2:     #removable
@@ -60,10 +111,11 @@ def get_removable_disk():
             hfile.Close()
     
 if __name__ == '__main__':
-    #disk_list = get_disk_info()
+    #get_disk_info()
+    get_removable_disk()
     #print(disk_list)
     #get_logic_info()
-    get_removable_disk()
+    #get_removable_disk()
     
     
 #data = os.popen("df -h").read()
