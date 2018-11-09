dfe3393979272c5125f669923fcd012994218741
labc <2367334102@qq.com>
2018-11-09 15:12:55 +0800

    add cp .

--- shell/03_kernel_build.sh
+++ shell/03_kernel_build.sh
@@ -6,4 +6,7 @@ rm -rf Build
 cd ./Kernel-4.9.110
 export ARCH=arm CROSS_COMPILE=arm-none-eabi-
 make O=../Build/Kernel jiaxing_defconfig
-make O=../Build/Kernel LOADADDR=0xA0008000 uImage dtbs -j4
+make O=../Build/Kernel LOADADDR=0xB8000000 uImage dtbs -j4
+cp ../Build/Kernel/arch/arm/boot/uImage /usr/tftp/labc/
+cp ../Build/Kernel/arch/arm/boot/dts/nextvpu_jiaxing_evb.dtb /usr/tftp/labc/
+ls -l /usr/tftp/labc/
